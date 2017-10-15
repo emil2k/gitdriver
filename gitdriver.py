1e35096d04763cd8316924b32d0e24ea48e485dd
Emil Davtyan <emil2k@gmail.com>
2017-10-15 12:25:22 +0200

    Use pygit2 instead of subprocess.

--- gitdriver.py
+++ gitdriver.py
@@ -6,6 +6,7 @@ import argparse
 import subprocess
 import shutil
 import yaml
+import pygit2 as git
 
 from drive import GoogleDrive, DRIVE_RW_SCOPE
 
@@ -23,45 +24,59 @@ def parse_args():
     p.add_argument('id', help='ID of document or folder')
     return p.parse_args()
 
-def process_file(gd, fid, filepath):
-    # Iterate over the revisions (from oldest to newest).
-    for rev in gd.revisions(fid):
-        with open(filepath, 'w') as fd:
-            if 'exportLinks' in rev and not opts.raw:
-                for mt in opts.mime_type:
-                    if mt in rev["exportLinks"]:
-                        r = gd.session.get(rev["exportLinks"][mt])
-                        break
-                if r is None:
-                    raise KeyError("mime type(s) %s not found in %s" % (opts.mime_type, rev["exportLinks"].keys()))
-            elif 'downloadUrl' in rev:
-                # Otherwise, if there is a downloadUrl, use that.
-                r = gd.session.get(rev['downloadUrl'])
-            else:
-                raise KeyError('unable to download revision')
-            # Write file content into local file.
-            for chunk in r.iter_content():
-                fd.write(chunk)
-        # Commit changes to repository.
-        subprocess.call(['git', 'add', filepath])
-        subprocess.call(['git', 'commit', '-m',
-            'revision from %s' % rev['modifiedDate']])
+class Processor:
 
-def process_folder(gd, fid, filepath):
-    os.mkdir(filepath)
-    # Process folder
-    for child in gd.children(fid):
-        process(gd, child["id"], filepath)
+    def __init__(self, gd, opts, repo):
+        self.gd = gd
+        self.opts = opts
+        self.repo = repo
+        self.tree_builder = repo.TreeBuilder()
+        self.last_commit = None
 
-def process(gd, rid, parent_path):
-    md = gd.get_file_metadata(rid)
-    filepath = os.path.join(parent_path, md["title"])
-    if md["mimeType"] == "application/vnd.google-apps.folder":
-        logging.info("Process folder: %s : %s : %s", rid, md["title"], filepath)
-        process_folder(gd, rid, filepath)
-    else:
-        logging.info("Process file: %s : %s : %s", rid, md["title"], filepath)
-        process_file(gd, rid, filepath)
+    def process_file(self, fid, filepath):
+        # Iterate over the revisions (from oldest to newest).
+        for rev in self.gd.revisions(fid):
+            with open(filepath, 'w') as fd:
+                if 'exportLinks' in rev and not self.opts.raw:
+                    for mt in self.opts.mime_type:
+                        if mt in rev["exportLinks"]:
+                            r = self.gd.session.get(rev["exportLinks"][mt])
+                            break
+                    if r is None:
+                        raise KeyError("mime type(s) %s not found in %s" % (self.opts.mime_type, rev["exportLinks"].keys()))
+                elif 'downloadUrl' in rev:
+                    # Otherwise, if there is a downloadUrl, use that.
+                    r = self.gd.session.get(rev['downloadUrl'])
+                else:
+                    raise KeyError('unable to download revision')
+                # Write file content into local file.
+                for chunk in r.iter_content():
+                    fd.write(chunk)
+            # Commit changes to repository.
+            self.repo.index.add(os.path.relpath(filepath, self.repo.workdir))
+            self.repo.index.write()
+            tree = self.repo.index.write_tree()
+            author = git.Signature("Unknown", "unknown email") # TODO use the last modifying user
+            parents = [] if self.last_commit is None else [self.last_commit]
+            message = 'revision from %s' % rev['modifiedDate']
+            self.last_commit = self.repo.create_commit("refs/heads/master", author, author, message, tree, parents)
+            logging.info("Commit: %s : %s", self.last_commit, message)
+
+    def process_folder(self, fid, filepath):
+        os.mkdir(filepath)
+        # Process folder
+        for child in self.gd.children(fid):
+            self.process(child["id"], filepath)
+
+    def process(self, rid, parent_path):
+        md = gd.get_file_metadata(rid)
+        filepath = os.path.join(parent_path, md["title"].lower().replace(" ", "_"))
+        if md["mimeType"] == "application/vnd.google-apps.folder":
+            logging.info("Process folder: %s : %s : %s", rid, md["title"], filepath)
+            self.process_folder(rid, filepath)
+        else:
+            logging.info("Process file: %s : %s : %s", rid, md["title"], filepath)
+            self.process_file(rid, filepath)
 
 if __name__ == '__main__':
     opts = parse_args()
@@ -86,10 +101,10 @@ if __name__ == '__main__':
     gd.authenticate()
 
     # Initialize the git repository.
-    logging.info("Creating output repsitory at %s", opts.output)
-    os.chdir(opts.output)
-    subprocess.call(['git','init'])
+    repo = git.init_repository(opts.output)
+    logging.info("Created output repository at %s", repo.workdir)
 
     # Begin processing.
-    process(gd, opts.id, opts.output)
+    p = Processor(gd, opts, repo)
+    p.process(opts.id, repo.workdir)
     logging.debug("Process complete!")
